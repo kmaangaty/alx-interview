e4dd0305fd546623dda5105cfe6667b03a5d1f0e
Kmangaty <kmaangaty@gmail.com>
2024-05-03 03:18:07 +0300

    ses 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,46 +1,17 @@
-#!/usr/bin/env python3
-"""
-LockBoxes Challenge
-"""
+#!/usr/bin/python3
+"""Script will unlock list of lists"""
 
 
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened or not.
-
-    Args:
-        boxes (list of list of int): A list of lists representing the boxes and their keys.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
-    num_boxes = len(boxes)
-    opened_boxes = set()
-    keys = set()
-    current_box = 0
 
-    while current_box < num_boxes:
-        opened_boxes.add(current_box)
-        keys.update(boxes[current_box])
-        next_box = None
-        for key in keys:
-            if key != current_box and key < num_boxes and key not in opened_boxes:
-                next_box = key
-                break
-        if next_box is not None:
-            current_box = next_box
-        else:
-            break
-
-    for box in range(num_boxes):
-        if box not in opened_boxes and box != 0:
-            return False
-    return True
-
-
-if __name__ == "__main__":
-    # Test cases
-    boxes1 = [[1], [2], [3], [4], []]
-
-    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-
-    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
