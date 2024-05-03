ad64cbc65c53798e6855e6d6263473ee74438a79
Kmangaty <kmaangaty@gmail.com>
2024-05-03 03:20:11 +0300

    ses 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Script will unlock list of lists"""
+"""
+Script to determine if all boxes can be unlocked.
+"""
 
 
 def canUnlockAll(boxes):
-    """This function will take a list of lists and the content
-       of a list will unlock other lists
-    """
+    """Determines if all the boxes can be unlocked.
 
+    Args:
+        boxes (list of int): A list of lists representing the boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    # Initialize the list of keys with the keys in the first box
     keys = [0]
+
+    # Traverse through the keys and explore other boxes
     for key in keys:
-        for boxKey in boxes[key]:
-            if boxKey not in keys and boxKey < len(boxes):
-                keys.append(boxKey)
+        # Check keys in the current box
+        for box_key in boxes[key]:
+            # If the key is new and within the range of boxes, add it to the list of keys
+            if box_key not in keys and box_key < len(boxes):
+                keys.append(box_key)
+
+    # If the number of keys equals the number of boxes, all boxes can be unlocked
     if len(keys) == len(boxes):
         return True
-    return False
+    else:
+        return False
