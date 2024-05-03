fb411e1c116cf1ab8dd6511673d1d311667c854a
Kmangaty <kmaangaty@gmail.com>
2024-05-03 03:28:40 +0300

    ses 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,18 +14,13 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
-    # Initialize the list of keys with the keys in the first box
     keys = [0]
 
-    # Traverse through the keys and explore other boxes
     for key in keys:
-        # Check keys in the current box
         for boxKey in boxes[key]:
-            # If the key is new and within the range of boxes, add it to the list of keys
             if boxKey not in keys and boxKey < len(boxes):
                 keys.append(boxKey)
 
-    # If the number of keys equals the number of boxes, all boxes can be unlocked
     if len(keys) == len(boxes):
         return True
     else:
