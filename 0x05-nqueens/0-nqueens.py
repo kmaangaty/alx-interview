8c5dda4147153f513fdfde5562f4372c547e3237
Kmangaty <kmaangaty@gmail.com>
2024-05-31 03:06:58 +0300

    ses 1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -56,31 +56,3 @@ def print_solutions(solutions):
     """
     for solution in solutions:
         print(solution)
-
-
-def main():
-    """
-    Main entry point for the script.
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [[0] * N for _ in range(N)]
-    solutions = solve_nqueens(board, 0)
-    for solution in solutions:
-        print(solution)
-
-
-if __name__ == "__main__":
-    main()
