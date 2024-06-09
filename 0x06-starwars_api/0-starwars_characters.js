d716732ebc4964c1ebb61ce4857fd4a4f3a843d7
Kmangaty <kmaangaty@gmail.com>
2024-06-09 16:26:18 +0300

    ses 1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,25 @@
 #!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-// Import the request module for making HTTP requests
-const httpRequest = require('request');
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-// Retrieve the movie ID from the command-line arguments
-const movieId = process.argv[2];
-
-// Construct the URL for the Star Wars API endpoint using the provided movie ID
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-// Make an asynchronous HTTP GET request to the Star Wars API
-httpRequest(apiUrl, async (error, response, body) => {
-  // Handle errors, if any
-  error && console.log(error);
-
-  // Extract the array of character URLs from the response body
-  const charactersArray = JSON.parse(response.body).characters;
-
-  // Iterate over each character URL and make an asynchronous request for their details
-  for (const characterUrl of charactersArray) {
-    // Use Promise to handle asynchronous request
-    await new Promise((resolve, reject) => {
-      // Make a request for each character URL
-      httpRequest(characterUrl, (error, response, body) => {
-        // Handle errors, if any
-        error && console.log(error);
-
-        // Parse the response body to extract the character's name and log it
-        console.log(JSON.parse(body).name);
-
-        // Resolve the Promise to continue with the next iteration
-        resolve();
-      });
-    });
-  }
-});
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
